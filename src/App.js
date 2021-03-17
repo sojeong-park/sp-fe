4dca4b94f742120df80c6a142936c3f283edcf90
So Jeong Park <psj6414@naver.com>
2021-03-17 21:54:57 +0900

    modify: main 문구수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          꼬북칩은 초코맛이 맛있어
         </p>
         <a
           className="App-link"
